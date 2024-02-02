4b081d6301d15dd4e7e263b157bbf26b3712031e
Nayanda Haberty <hainayanda@outlook.com>
2024-02-02 09:43:04 +0700

    Add spacer tile

---
+++ MosaicGrid/Classes/Implementation/SpacerTile.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceTile.swift
+//  MosaicGrid
+//
+//  Created by Nayanda Haberty on 2/2/24.
+//
+
+import Foundation
+import SwiftUI
+
+// swiftlint:disable identifier_name
+public func SpaceTile(w width: Int = 1, h height: Int = 1) -> some View {
+    Rectangle()
+        .foregroundColor(.clear)
+        .tileSized(w: width, h: height)
+}
+// swiftlint:enable identifier_name
