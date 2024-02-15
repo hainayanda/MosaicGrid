a3676bbe48b3bda1dd23e04efdafba52cbd4dd38
Nayanda Haberty <hainayanda@outlook.com>
2024-02-15 16:28:36 +0700

    Update workflows

--- .github/scripts/test_lib.sh
+++
@@ -1,6 +0,0 @@
-set -eo pipefail
-
-xcodebuild -workspace Example/MosaicGrid.xcworkspace \
-            -scheme MosaicGrid-Example \
-            -destination platform=iOS\ Simulator,OS=16.4,name=iPhone\ 14 \
-            clean test | xcpretty
