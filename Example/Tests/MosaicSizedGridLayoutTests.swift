5d45ca63b7b2de2e68973c56587032b98bff59ad
Nayanda Haberty <hainayanda@outlook.com>
2024-03-13 20:09:59 +0700

    Fix unit test

--- Example/Tests/MosaicSizedGridLayoutTests.swift
+++ Example/Tests/MosaicSizedGridLayoutTests.swift
@@ -16,12 +16,12 @@ class MosaicSizedGridLayoutTests: XCTestCase {
     func test_givenVGridWithSameSize_whenCalculateTileSize_shouldReturnSameSize() {
         let vGrid = MosaicSizedGridLayout(
             orientation: .vertical,
-            tileSize: CGSize(width: 10, height: 10)
+            gridSize: CGSize(width: 10, height: 10)
         )
         
         let viewSize = ProposedViewSize(width: 10, height: 10)
         
-        let size = vGrid.tilesSize(basedOn: viewSize)
+        let size = vGrid.calculateGridSize(basedOn: viewSize)
         
         XCTAssertEqual(size, CGSize(width: 10, height: 10))
     }
@@ -29,12 +29,12 @@ class MosaicSizedGridLayoutTests: XCTestCase {
     func test_givenHGridWithSameSize_whenCalculateTileSize_shouldReturnSameSize() {
         let hGrid = MosaicSizedGridLayout(
             orientation: .horizontal,
-            tileSize: CGSize(width: 10, height: 10)
+            gridSize: CGSize(width: 10, height: 10)
         )
         
         let viewSize = ProposedViewSize(width: 10, height: 10)
         
-        let size = hGrid.tilesSize(basedOn: viewSize)
+        let size = hGrid.calculateGridSize(basedOn: viewSize)
         
         XCTAssertEqual(size, CGSize(width: 10, height: 10))
     }
@@ -42,12 +42,12 @@ class MosaicSizedGridLayoutTests: XCTestCase {
     func test_givenVGridWithZeroSpacing_whenCalculateTileSize_shouldReturnDividedTileDimension() {
         let vGrid = MosaicSizedGridLayout(
             orientation: .vertical,
-            tileSize: CGSize(width: 10, height: 10)
+            gridSize: CGSize(width: 10, height: 10)
         )
         
         let viewSize = ProposedViewSize(width: 50, height: 50)
         
-        let size = vGrid.tilesSize(basedOn: viewSize)
+        let size = vGrid.calculateGridSize(basedOn: viewSize)
         
         XCTAssertEqual(size, CGSize(width: 10, height: 10))
     }
@@ -55,12 +55,12 @@ class MosaicSizedGridLayoutTests: XCTestCase {
     func test_givenHGridWithZeroSpacing_whenCalculateTileSize_shouldReturnDividedTileDimension() {
         let hGrid = MosaicSizedGridLayout(
             orientation: .horizontal,
-            tileSize: CGSize(width: 10, height: 10)
+            gridSize: CGSize(width: 10, height: 10)
         )
         
         let viewSize = ProposedViewSize(width: 50, height: 50)
         
-        let size = hGrid.tilesSize(basedOn: viewSize)
+        let size = hGrid.calculateGridSize(basedOn: viewSize)
         
         XCTAssertEqual(size, CGSize(width: 10, height: 10))
     }
@@ -68,13 +68,13 @@ class MosaicSizedGridLayoutTests: XCTestCase {
     func test_givenVGridWithSpacing_whenCalculateTileSize_shouldReturnDividedTileDimensionWithSpacingIntoAccount() {
         let vGrid = MosaicSizedGridLayout(
             orientation: .vertical,
-            tileSize: CGSize(width: 10, height: 10),
-            minimumSpacing: 10
+            gridSize: CGSize(width: 10, height: 10),
+            minimumSpacing: MosaicGridSpacing(spacings: 10)
         )
         
         let viewSize = ProposedViewSize(width: 90, height: 90)
         
-        let size = vGrid.tilesSize(basedOn: viewSize)
+        let size = vGrid.calculateGridSize(basedOn: viewSize)
         
         XCTAssertEqual(size, CGSize(width: 10, height: 10))
     }
@@ -82,13 +82,13 @@ class MosaicSizedGridLayoutTests: XCTestCase {
     func test_givenHGridWithSpacing_whenCalculateTileSize_shouldReturnDividedTileDimensionWithSpacingIntoAccount() {
         let hGrid = MosaicSizedGridLayout(
             orientation: .horizontal,
-            tileSize: CGSize(width: 10, height: 10),
-            minimumSpacing: 10
+            gridSize: CGSize(width: 10, height: 10),
+            minimumSpacing: MosaicGridSpacing(spacings: 10)
         )
         
         let viewSize = ProposedViewSize(width: 90, height: 90)
         
-        let size = hGrid.tilesSize(basedOn: viewSize)
+        let size = hGrid.calculateGridSize(basedOn: viewSize)
         
         XCTAssertEqual(size, CGSize(width: 10, height: 10))
     }
@@ -96,13 +96,13 @@ class MosaicSizedGridLayoutTests: XCTestCase {
     func test_givenVGridWithZeroProposal_whenCalculateTileSize_shouldReturnZero() {
         let vGrid = MosaicSizedGridLayout(
             orientation: .vertical,
-            tileSize: CGSize(width: 9, height: 9),
-            minimumSpacing: 10
+            gridSize: CGSize(width: 9, height: 9),
+            minimumSpacing: MosaicGridSpacing(spacings: 10)
         )
         
         let viewSize = ProposedViewSize(width: 0, height: 0)
         
-        let size = vGrid.tilesSize(basedOn: viewSize)
+        let size = vGrid.calculateGridSize(basedOn: viewSize)
         
         XCTAssertEqual(size, CGSize(width: 0, height: 0))
     }
@@ -110,13 +110,13 @@ class MosaicSizedGridLayoutTests: XCTestCase {
     func test_givenHGridWithZeroProposal_whenCalculateTileSize_shouldReturnZero() {
         let hGrid = MosaicSizedGridLayout(
             orientation: .horizontal,
-            tileSize: CGSize(width: 9, height: 9),
-            minimumSpacing: 10
+            gridSize: CGSize(width: 9, height: 9),
+            minimumSpacing: MosaicGridSpacing(spacings: 10)
         )
         
         let viewSize = ProposedViewSize(width: 0, height: 0)
         
-        let size = hGrid.tilesSize(basedOn: viewSize)
+        let size = hGrid.calculateGridSize(basedOn: viewSize)
         
         XCTAssertEqual(size, CGSize(width: 0, height: 0))
     }
