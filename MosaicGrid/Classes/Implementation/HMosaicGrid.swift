908563234ee5fb1197127249b8c0c2a159cd9df9
Nayanda Haberty <hainayanda@outlook.com>
2024-10-03 22:43:31 +0700

    Add FlowGridLayout to HMosaicGrid and VMosaicGrid

--- MosaicGrid/Classes/Implementation/HMosaicGrid.swift
+++ MosaicGrid/Classes/Implementation/HMosaicGrid.swift
@@ -13,6 +13,21 @@ public struct HMosaicGrid<Content>: View where Content: View {
     private let underlyingMosaicGrid: MosaicGrid<Content>
     
     /// Initialize Horizontal Mosaic Grid View.
+    /// It will try to arrange a grid without helping of the grid guide.
+    /// But it will just arrange the subviews where it will fit with a given `spacing`
+    /// - Parameters:
+    ///   - spacing: Spacing on each item.
+    ///   - content: View that will be used as this Mosaic Grid Content
+    public init(spacing: MosaicGridSpacing, @ViewBuilder content: @escaping () -> Content) {
+        self.underlyingMosaicGrid = MosaicGrid(
+            orientation: .horizontal,
+            spacing: spacing,
+            gridSizing: .flow,
+            content: content
+        )
+    }
+    
+    /// Initialize Horizontal Mosaic Grid View.
     /// It will divide the height of the view with `vGridCount` to get the height of a single grid.
     /// If `spacing` is provided, it will add it to the calculation to make sure each grid has a spacing from each other.
     /// `gridAspectRatio` then will be used to calculate the width of a single grid.
@@ -75,13 +90,40 @@ public struct HMosaicGrid<Content>: View where Content: View {
 public extension HMosaicGrid {
     
     /// Initialize Horizontal Mosaic Grid View.
+    /// It will try to arrange a grid without helping of the grid guide.
+    /// But it will just arrange the subviews where it will fit with a given `spacing`
+    /// - Parameters:
+    ///   - spacing: Both horizontal and vertical spacing on each item.
+    ///   - content: View that will be used as this Mosaic Grid Content
+    @inlinable init(spacing: CGFloat, @ViewBuilder content: @escaping () -> Content) {
+        self.init(
+            spacing: MosaicGridSpacing(h: spacing, v: spacing),
+            content: content
+        )
+    }
+    
+    /// Initialize Horizontal Mosaic Grid View.
+    /// It will try to arrange a grid without helping of the grid guide.
+    /// But it will just arrange the subviews where it will fit with a given `spacing`
+    /// - Parameters:
+    ///   - hSpacing: Horizontal spacing on each item.
+    ///   - vSpacing: Vertical spacing on each item.
+    ///   - content: View that will be used as this Mosaic Grid Content
+    @inlinable init(hSpacing: CGFloat = .zero, vSpacing: CGFloat = .zero, @ViewBuilder content: @escaping () -> Content) {
+        self.init(
+            spacing: MosaicGridSpacing(h: hSpacing, v: vSpacing),
+            content: content
+        )
+    }
+    
+    /// Initialize Horizontal Mosaic Grid View.
     /// It will divide the height of the view with `vGridCount` to get the height of a single grid.
     /// If `spacing` is provided, it will add it to the calculation to make sure each grid has a spacing from each other.
     /// `gridAspectRatio` then will be used to calculate the width of a single grid.
     /// `content` will be used to get the view to be arranged by the Mosaic Grid View
     /// - Parameters:
     ///   - vGridCount: Count of the vertical grid.
-    ///   - spacing: Both horizontal and vertical spacing on each grid. 
+    ///   - spacing: Both horizontal and vertical spacing on each grid.
     ///   - gridAspectRatio: Aspect ratio of each grid
     ///   - content: View that will be used as this Mosaic Grid Content
     @inlinable init(vGridCount: Int, spacing: CGFloat, gridAspectRatio: Double = 1, @ViewBuilder content: @escaping () -> Content) {
@@ -100,8 +142,8 @@ public extension HMosaicGrid {
     /// `content` will be used to get the view to be arranged by the Mosaic Grid View
     /// - Parameters:
     ///   - vGridCount: Count of the vertical grid.
-    ///   - hSpacing: Horizontal spacing on each grid. 
-    ///   - vSpacing: Vertical spacing on each grid. 
+    ///   - hSpacing: Horizontal spacing on each grid.
+    ///   - vSpacing: Vertical spacing on each grid.
     ///   - gridAspectRatio: Aspect ratio of each grid
     ///   - content: View that will be used as this Mosaic Grid Content
     @inlinable init(vGridCount: Int, hSpacing: CGFloat = .zero, vSpacing: CGFloat = .zero, gridAspectRatio: Double = 1, @ViewBuilder content: @escaping () -> Content) {
@@ -120,7 +162,7 @@ public extension HMosaicGrid {
     /// `content` will be used to get the view to be arranged by the Mosaic Grid View
     /// - Parameters:
     ///   - vGridCount: Count of the vertical grid.
-    ///   - spacing: Both horizontal and vertical spacing on each grid. 
+    ///   - spacing: Both horizontal and vertical spacing on each grid.
     ///   - gridWidth: Width of each grid
     ///   - content: View that will be used as this Mosaic Grid Content
     @inlinable init(vGridCount: Int, spacing: CGFloat, gridWidth: CGFloat, @ViewBuilder content: @escaping () -> Content) {
@@ -139,8 +181,8 @@ public extension HMosaicGrid {
     /// `content` will be used to get the view to be arranged by the Mosaic Grid View
     /// - Parameters:
     ///   - vGridCount: Count of the vertical grid.
-    ///   - hSpacing: Horizontal spacing on each grid. 
-    ///   - vSpacing: Vertical spacing on each grid. 
+    ///   - hSpacing: Horizontal spacing on each grid.
+    ///   - vSpacing: Vertical spacing on each grid.
     ///   - gridWidth: Width of each grid
     ///   - content: View that will be used as this Mosaic Grid Content
     @inlinable init(vGridCount: Int, hSpacing: CGFloat = .zero, vSpacing: CGFloat = .zero, gridWidth: CGFloat, @ViewBuilder content: @escaping () -> Content) {
@@ -158,7 +200,7 @@ public extension HMosaicGrid {
     /// `content` will be used to get the view to be arranged by the Mosaic Grid View
     /// - Parameters:
     ///   - gridSize: Static size of a single grid.
-    ///   - minimumSpacing: Both horizontal and vertical minimum spacing on each grid. 
+    ///   - minimumSpacing: Both horizontal and vertical minimum spacing on each grid.
     ///   - content: the view that will be used as this Mosaic Grid Content
     @inlinable init(gridSize: CGSize, minimumSpacing: CGFloat, @ViewBuilder content: @escaping () -> Content) {
         self.init(
@@ -174,8 +216,8 @@ public extension HMosaicGrid {
     /// `content` will be used to get the view to be arranged by the Mosaic Grid View
     /// - Parameters:
     ///   - gridSize: Static size of a single grid.
-    ///   - minimumHSpacing: Horizontal minimum spacing on each grid. 
-    ///   - minimumVSpacing: Vertical minimum spacing on each grid. 
+    ///   - minimumHSpacing: Horizontal minimum spacing on each grid.
+    ///   - minimumVSpacing: Vertical minimum spacing on each grid.
     ///   - content: the view that will be used as this Mosaic Grid Content
     @inlinable init(gridSize: CGSize, minimumHSpacing: CGFloat = .zero, minimumVSpacing: CGFloat = .zero, @ViewBuilder content: @escaping () -> Content) {
         self.init(
@@ -188,44 +230,96 @@ public extension HMosaicGrid {
 
 // MARK: Preview
 #if DEBUG
-struct HMosaicGrid_Previews: PreviewProvider {
-    static var previews: some View {
-        ScrollView(.horizontal) {
-            HMosaicGrid(vGridCount: 6, spacing: 10) {
-                ForEach(0..<50) { _ in
-                    Rectangle()
-                        .foregroundColor(.red)
-                        .usingGrids(h: 2, v: 2)
-                    Rectangle()
-                        .foregroundColor(.orange)
-                        .usingGrids(h: 2)
-                    Rectangle()
-                        .foregroundColor(.yellow)
-                        .usingGrids(v: 3)
-                    Rectangle()
-                        .foregroundColor(.green)
-                        .usingGrids()
-                    Rectangle()
-                        .foregroundColor(.cyan)
-                        .usingGrids(h: 3, v: 2)
-                    Rectangle()
-                        .foregroundColor(.blue)
-                        .usingGrids()
-                    Rectangle()
-                        .foregroundColor(.purple)
-                        .usingGrids(h: 2, v: 3)
-                    Rectangle()
-                        .foregroundColor(.pink)
-                        .usingGrids(h: 2)
-                    Rectangle()
-                        .foregroundColor(.red)
-                        .usingGrids(v: 2)
-                    Rectangle()
-                        .foregroundColor(.orange)
-                        .usingGrids(v: 3)
-                }
+#Preview(".flow") {
+    ScrollView(.horizontal) {
+        HMosaicGrid(spacing: 10) {
+            ForEach(0..<10) { _ in
+                Rectangle()
+                    .foregroundColor(.red)
+                    .frame(width: 200, height: 200)
+                Rectangle()
+                    .foregroundColor(.orange)
+                    .frame(width: 200, height: 100)
+                Rectangle()
+                    .foregroundColor(.yellow)
+                    .frame(width: 100, height: 300)
+                Rectangle()
+                    .foregroundColor(.green)
+                    .frame(width: 100, height: 100)
+                Rectangle()
+                    .foregroundColor(.cyan)
+                    .frame(width: 90, height: 410)
             }
-            .padding()
+        }
+        .padding()
+    }
+}
+#Preview(".aspectRatio") {
+    ScrollView(.horizontal) {
+        HMosaicGrid(vGridCount: 6, spacing: 10) {
+            ForEach(0..<10) { _ in
+                Rectangle()
+                    .foregroundColor(.red)
+                    .usingGrids(h: 2, v: 2)
+                Rectangle()
+                    .foregroundColor(.orange)
+                    .usingGrids(h: 2)
+                Rectangle()
+                    .foregroundColor(.yellow)
+                    .usingGrids(v: 3)
+                Rectangle()
+                    .foregroundColor(.green)
+                    .usingGrids()
+                Rectangle()
+                    .foregroundColor(.cyan)
+                    .usingGrids(h: 1, v: 2)
+            }
+        }
+    }
+}
+#Preview(".constantSize") {
+    ScrollView(.horizontal) {
+        HMosaicGrid(gridSize: CGSize(width: 120, height: 120), minimumSpacing: 10) {
+            ForEach(0..<10) { _ in
+                Rectangle()
+                    .foregroundColor(.red)
+                    .usingGrids(h: 2, v: 2)
+                Rectangle()
+                    .foregroundColor(.orange)
+                    .usingGrids(h: 2)
+                Rectangle()
+                    .foregroundColor(.yellow)
+                    .usingGrids(v: 3)
+                Rectangle()
+                    .foregroundColor(.green)
+                    .usingGrids()
+                Rectangle()
+                    .foregroundColor(.cyan)
+                    .usingGrids(h: 1, v: 2)
+            }
+        }
+    }
+}
+#Preview(".constantAxis") {
+    ScrollView(.horizontal) {
+        HMosaicGrid(vGridCount: 6, spacing: 10, gridWidth: 120) {
+            ForEach(0..<10) { _ in
+                Rectangle()
+                    .foregroundColor(.red)
+                    .usingGrids(h: 2, v: 2)
+                Rectangle()
+                    .foregroundColor(.orange)
+                    .usingGrids(h: 2)
+                Rectangle()
+                    .foregroundColor(.yellow)
+                    .usingGrids(v: 3)
+                Rectangle()
+                    .foregroundColor(.green)
+                    .usingGrids()
+                Rectangle()
+                    .foregroundColor(.cyan)
+                    .usingGrids(h: 1, v: 2)
+            }
         }
     }
 }
