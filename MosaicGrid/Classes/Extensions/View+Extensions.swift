4d8754343964f51408d54d6bcdd693243bf98c5c
Nayanda Haberty <hainayanda@outlook.com>
2024-10-04 22:38:27 +0700

    Remove deprecation

--- MosaicGrid/Classes/Extensions/View+Extensions.swift
+++ MosaicGrid/Classes/Extensions/View+Extensions.swift
@@ -9,15 +9,6 @@ import Foundation
 import SwiftUI
 
 extension View {
-    /// Set the size of the tiles based on the number of the grid used inside `VMosaicGrid` or `HMosaicGrid`.
-    /// - Parameters:
-    ///   - width: Number of horizontal grid used. Default is 1.
-    ///   - height: Number of vertical grid used. Default is 1.
-    /// - Returns: View with tile size layout value.
-    @available(*, deprecated, renamed: "usingGrids(h:v:)")
-    public func tileSized(w width: Int = 1, h height: Int = 1) -> some View {
-        return layoutValue(key: UsingGrids.self, value: MosaicGridSize(width: width, height: height))
-    }
     
     /// Set the size of the tiles based on the number of the tiles used inside `VMosaicGrid` or `HMosaicGrid`.
     /// Keep in mind that this will not resize the view inside it. It will only proposed this size to the particular view.
--- MosaicGrid/Classes/Implementation/SpacerTile.swift
+++ MosaicGrid/Classes/Implementation/SpacerTile.swift
@@ -11,38 +11,6 @@ import SwiftUI
 // swiftlint:disable identifier_name
 /// Create a clear rectangle with a given tile size.
 /// - Parameters:
-///   - width: number of horizontal tiles used.
-///   - height: number of vertical tiles used.
-/// - Returns: View with given tile size
-@available(*, deprecated, renamed: "SpacerTile(h:v:)")
-public func SpacerTile(w width: Int, h height: Int) -> some View {
-    Rectangle()
-        .foregroundColor(.clear)
-        .tileSized(w: width, h: height)
-}
-
-/// Create a clear rectangle with a given tile size.
-/// - Parameter width: number of horizontal tiles used.
-/// - Returns: View with given tile size
-@available(*, deprecated, renamed: "SpacerTile(h:v:)")
-public func SpacerTile(w width: Int) -> some View {
-    Rectangle()
-        .foregroundColor(.clear)
-        .tileSized(w: width, h: 1)
-}
-
-/// Create a clear rectangle with a given tile size.
-/// - Parameter height: number of vertical tiles used.
-/// - Returns: View with given tile size
-@available(*, deprecated, renamed: "SpacerTile(h:v:)")
-public func SpacerTile(h height: Int) -> some View {
-    Rectangle()
-        .foregroundColor(.clear)
-        .tileSized(w: 1, h: height)
-}
-
-/// Create a clear rectangle with a given tile size.
-/// - Parameters:
 ///   - horizontal: number of horizontal tiles used.
 ///   - vertical: number of vertical tiles used.
 /// - Returns: View with given tile size
